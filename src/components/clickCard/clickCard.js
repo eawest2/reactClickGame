2cf4b278b0589ac43199170507b6bc5b601d4920
eawest2 <eawest2@gmail.com>
2018-08-09 12:18:42 -0400

    updated lock file

--- src/components/clickCard/clickCard.js
+++ src/components/clickCard/clickCard.js
@@ -4,7 +4,7 @@ import "./clickCard.css";
 const clickCard = props => (
   <div className="card">
     <div className="img-container card-img">
-      <img alt={props.name} src={props.image} data-id={props.id} onClick={() => props.removeCard(props.id)}/>
+      <img alt={props.name} src={props.image} data-id={props.id} onClick={() => props.removeCard(props.id)} />
     </div>
   </div>
 );
